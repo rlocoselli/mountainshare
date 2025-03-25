1f5219d6965d2db400aea853e2611d89b34eb035
rlocoselli <rlocoselli@yahoo.com.br>
2025-03-25 22:07:53 +0100

    Adding Accordion and Carousel

--- Mountainshare/ProfilePage.xaml.cs
+++ Mountainshare/ProfilePage.xaml.cs
@@ -1,12 +1,22 @@
 using Microsoft.Maui.Controls;
+using System.Collections.ObjectModel;
 
 namespace OutdoorShareMauiApp.Pages
 {
     public partial class ProfilePage : ContentPage
     {
+        public ObservableCollection<string> Images { get; set; }
+
         public ProfilePage()
         {
             InitializeComponent();
+
+            Images = new ObservableCollection<string>
+            {
+                @"http://www.meubusao.com/images/ic_launcher2.png",
+            };
+
+            BindingContext = Images;
         }
     }
 }
